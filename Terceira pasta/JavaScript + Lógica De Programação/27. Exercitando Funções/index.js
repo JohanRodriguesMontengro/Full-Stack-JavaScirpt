77e4ca86d1220c779067d9ca49aec33fe83ec010
Johan <ti.johan@ti7.com.br>
2022-08-09 16:43:30 -0300

    finalizando

--- Terceira pasta/JavaScript + Lógica De Programação/27. Exercitando Funções/index.js
+++ Terceira pasta/JavaScript + Lógica De Programação/27. Exercitando Funções/index.js
@@ -1,14 +1,14 @@
 let ship = {
     shipName: prompt(`Qual é o nome da sua nave piloto?: `),
     velocity: 0
-  }
-  let chosenOption = ``
-  
-  function speedUp() {
+}
+let chosenOption = ``
+
+function speedUp() {
     alert(`Acelerando mais 5km/s de velocidade!`)
     return ship.velocity += 5
-  }
-  function speedDown() {
+}
+function speedDown() {
     if (velocity <= 0) {
       alert(`Você está parado, não é possível desacelerar!`)
       return ship.velocity = 0
@@ -16,35 +16,32 @@ let ship = {
       alert(`Desacelerando mais 5km/s de velocidade!`)
       return ship.velocity -= 5
     }
-  }
-  
-  function printInfo() {
+}
+function printInfo() {
     alert(`O nome da sua nave é: ${ship.shipName}\nSua velocidade atual é de: ${ship.velocity}km/s`)
-  }
-  
-  function exit() {
+}
+function exit() {
     alert(`Finalizando o programa...`)
-  }
-  
-  do {
+}
+do{
     chosenOption = prompt(
-      `Escolha uma opção\n1 - Acelerar a nave em 5 km/h\n2 - Desacelerar a nave em 5 km/h\n3 - Imprimir dados de bordo\n4 - Sair do programa`)
-      
+    `Escolha uma opção\n1 - Acelerar a nave em 5 km/h\n2 - Desacelerar a nave em 5 km/h\n3 - Imprimir dados de bordo\n4 - Sair do programa\n\nVelocidade atual: ${ship.velocity}`)
+
     switch (chosenOption) {
-      case '1':
+    case '1':
         velocity = speedUp()
-      break;
-      case '2':
-        velocity = speedDown()
-      break
-      case '3':
-        printInfo()
-      break;
-      case '4':
-        exit()
-      break;
-      default:
-        alert(`Escolha uma opção valida!`)
         break;
-    }
-  } while (chosenOption != 4);
+    case '2':
+        velocity = speedDown()
+        break;
+    case '3':
+        printInfo()
+        break;
+    case '4':
+        exit()
+        break;
+    default:
+      alert(`Escolha uma opção valida!`)
+      break;
+  }
+}while (chosenOption != 4);
