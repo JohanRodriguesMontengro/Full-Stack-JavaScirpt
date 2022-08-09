aa445cb2ad87a1c4209d261768acf57e01b72884
Johan <ti.johan@ti7.com.br>
2022-08-09 15:15:31 -0300

    Update index.js

--- Terceira pasta/JavaScript + Lógica De Programação/27. Exercitando Funções/index.js
+++ Terceira pasta/JavaScript + Lógica De Programação/27. Exercitando Funções/index.js
@@ -1,11 +1,16 @@
 let shipName = prompt('Qual é o nome da nave?')
 let shipVelocity = 0
+let newShipVelocity = 0
 
-let chosenOption = prompt('1- Acelerar a nave em 5km/s\n2- Desacelerar em 5km/s\n3- Imprimir dados de bordo\n4- Sair do programa')
+let numberChosen = prompt('1- Acelerar a nave em 5km/s\n2- Desacelerar em 5km/s\n3- Imprimir dados de bordo\n4- Sair do programa\n\nVelocidade atual da nave: ' + newShipVelocity) 
 
-function acelerar(shipVelocity) {
-    shipVelocity += 5
-}
-function desacelerar(shipVelocity) {
-    shipVelocity -= 5
+while(numberChosen == 1 || 2){
+    
+    function acelerar(_velocity = 5) {
+        newShipVelocity = shipVelocity + 5
+        alert('Você acelerou em 5km/s')
+    }
+    function desacelerar(_velocity = -5) {
+        newShipVelocity = shipVelocity - 5
+    }
 }
