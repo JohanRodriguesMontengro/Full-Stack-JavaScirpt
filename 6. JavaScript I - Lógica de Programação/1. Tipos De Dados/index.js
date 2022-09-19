b6fb4f7ff1b39488196c406efbb7dc6a27f42b64
Johan <ti.johan@ti7.com.br>
2022-09-19 15:46:16 -0300

    start day

---
+++ 6. JavaScript I - Lógica de Programação/1. Tipos De Dados/index.js
@@ -0,0 +1,40 @@
+/*
+Números
+5
+999
+82.5
+
+Bigint
+999999999999999999999999999999999999999999999999999999999999n
+3n
+10n
+
+String
+"Issac"
+'Isaac'
+`Isaac`
+"42" =! 42
+
+"Isaac"[0] = "I"
+"Isaac"[4] = "c"
+"Isaac".length
+
+Boolean
+false
+true
+
+Valores Nulos
+null
+undefined
+
+Objetos
+typeof console
+typeof "Isaac"
+typeof window
+
+Funções
+typeof console.error
+console.error("Algo deu errado")
+console.warn("Aviso")
+window.alert("Isso é um alerta")
+*/
