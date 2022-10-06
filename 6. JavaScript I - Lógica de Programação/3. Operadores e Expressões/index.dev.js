df5fbcf6e261562faa97bafa470a0bfdf06d9cc3
Johan <ti.johan@ti7.com.br>
2022-10-06 14:02:48 -0300

    start day

---
+++ 6. JavaScript I - Lógica de Programação/3. Operadores e Expressões/index.dev.js
@@ -0,0 +1,19 @@
+"use strict";
+
+1 + 1;
+7 - 2;
+3 * 3;
+6 / 2;
+6 % 2;
+3 + 3 * 5(3 + 3) * 5;
+var npme = "Isaac";
+var number = 5;
+number = 3;
+number = number + 3;
+number += 3;
+number -= 2;
+console.error("ERROR");
+number++;
+number--;
+"Issac" + " Pontes";
+1 + "1";
---
+++ 6. JavaScript I - Lógica de Programação/3. Operadores e Expressões/index.js
@@ -0,0 +1,23 @@
+1 + 1
+7 - 2
+3 * 3
+6 / 2
+6 % 2
+
+3 + 3 * 5
+(3 + 3) * 5
+
+let npme = `Isaac`
+let number = 5
+number = 3
+number = number + 3
+number += 3
+number -= 2
+
+console.error(`ERROR`)
+
+number++
+number--
+
+`Issac` + ` Pontes`
+1 + `1`
