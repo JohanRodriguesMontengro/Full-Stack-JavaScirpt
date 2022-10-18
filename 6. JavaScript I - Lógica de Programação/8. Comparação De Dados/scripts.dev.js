28462fcd36631072917c1507970f81aa2e710e16
Johan <ti.johan@ti7.com.br>
2022-10-18 15:28:48 -0300

    5 commit

---
+++ 6. JavaScript I - Lógica de Programação/8. Comparação De Dados/scripts.dev.js
@@ -0,0 +1,35 @@
+"use strict";
+
+3 == 3; // Retorna: true
+
+4 == 3; // Retorna false
+
+3 * 3 + 1 == 10; // Retorna: true
+
+10 === 10; // Retorna: true
+
+10 === "10"; // Retorna: false
+
+3 != 10; // Retorna: true
+
+3 != 3; // Retorna: false
+
+5 != "5"; // Retorna: false
+
+5 !== '5'; // Retorna: true
+
+3 > 1; // Retorna: true
+
+3 > 4; // Retorna: false
+
+1 < 0; // Retorna: false
+
+1 < 9; // Retorna: true
+
+3 >= 3; // Retorna: true
+
+3 >= 4; // Retorna: false
+
+3 <= 0; // Retorna: false
+
+3 <= 4; // Retorna: true
