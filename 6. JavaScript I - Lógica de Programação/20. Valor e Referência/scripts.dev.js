8aebc3d490c194f3d5cbbd49269678b3190b0c25
Johan <ti.johan@ti7.com.br>
2022-10-31 14:24:46 -0300

    start day

---
+++ 6. JavaScript I - Lógica de Programação/20. Valor e Referência/scripts.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var a = 5;
+var b = a;
+b = 7;
+var array = [1, 4, 6, 2];
+var array2 = array;
+array2[0] = 5;
---
+++ 6. JavaScript I - Lógica de Programação/20. Valor e Referência/scripts.js
@@ -0,0 +1,11 @@
+let a = 5
+
+let b = a
+
+b = 7
+
+let array = [1, 4, 6, 2]
+
+let array2 = array
+
+array2[0] = 5
