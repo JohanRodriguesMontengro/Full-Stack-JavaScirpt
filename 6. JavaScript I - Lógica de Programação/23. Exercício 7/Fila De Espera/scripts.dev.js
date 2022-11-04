a2d9591e3619249a528af4c9fed53852ad0b60ff
Johan <ti.johan@ti7.com.br>
2022-11-04 14:36:28 -0300

    start day

--- 6. JavaScript I - Lógica de Programação/23. Exercício 7/Fila De Espera/scripts.dev.js
+++ 6. JavaScript I - Lógica de Programação/23. Exercício 7/Fila De Espera/scripts.dev.js
@@ -1,7 +1,22 @@
 "use strict";
 
 var Continuar = true;
+var Number = 1;
+var ArrayNumber = "";
+var ArrayPacientes;
+var FilaDeEspera = "";
 
 do {
-  var FilaDeEspera = prompt("Fila De Espera:");
+  ArrayNumber = [Number + "\xBA"];
+  ArrayPacientes = ["".concat(ArrayNumber, "Matheus")];
+  FilaDeEspera = prompt("Fila De Espera:\n  ".concat(ArrayPacientes, "\n  \n  1- Adicionar pacientes"));
+  FilaDeEspera = parseInt(FilaDeEspera);
+
+  if (FilaDeEspera === 1) {
+    Number++;
+  }
+
+  Continuar = false;
 } while (Continuar === true);
+
+console.log(FilaDeEspera);
--- 6. JavaScript I - Lógica de Programação/23. Exercício 7/Fila De Espera/scripts.js
+++ 6. JavaScript I - Lógica de Programação/23. Exercício 7/Fila De Espera/scripts.js
@@ -1,5 +1,22 @@
 let Continuar = true
+let Number = 1
+let ArrayNumber = ``
+let ArrayPacientes
+let FilaDeEspera = ``
 
 do {  
-  let FilaDeEspera = prompt(`Fila De Espera:`)
-}while(Continuar === true)
+  ArrayNumber = [Number + `º`]
+  ArrayPacientes = [`${ArrayNumber}Matheus`]
+  FilaDeEspera = prompt(`Fila De Espera:
+  ${ArrayPacientes}
+  
+  1- Adicionar pacientes`)
+
+  FilaDeEspera = parseInt(FilaDeEspera)
+  if (FilaDeEspera === 1) {
+    Number++
+    
+  }
+  Continuar = false
+} while (Continuar === true)
+console.log(FilaDeEspera)
