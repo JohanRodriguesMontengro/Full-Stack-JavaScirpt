c582994bd7680580bb83e99e80eb9781ae9ab58a
Johan <ti.johan@ti7.com.br>
2022-11-08 14:34:59 -0300

    start day

--- 6. JavaScript I - Lógica de Programação/26. Exercício 8/Cadastro De Imóveis/scripts.dev.js
+++ 6. JavaScript I - Lógica de Programação/26. Exercício 8/Cadastro De Imóveis/scripts.dev.js
@@ -11,20 +11,35 @@ do {
     BanheirosImovel: "",
     GaragemImovel: ""
   };
-  Opcao = prompt("Quantidades de im\xF3veis cadastrados: ".concat(ImoveisCadastrados, "\n  1- Cadastrar Im\xF3veis\n  2- Mostrar im\xF3veis cadastrados\n  3- Sair\n  "));
+  Opcao = prompt("Quantidades de im\xF3veis cadastrados: ".concat(ImoveisCadastrados, "\n  1- Cadastrar Im\xF3veis;\n  2- Mostrar im\xF3veis cadastrados;\n  3- Sair.\n  "));
 
   switch (Opcao) {
     case "1":
-      var NomeDonoImovel = prompt("Digite o nome do dono do im\xF3vel");
-      var QuartosImovel = prompt("Quantos quartos o im\xF3vel possui");
-      var BanheirosImovel = prompt("Quantos banheiros o im\xF3vel possui");
-      var GaragemImovel = prompt("O im\xF3vel possui uma garagem?\n    1- Sim\n    2- N\xE3o");
-      NomeImovel.push(prompt("Informe o nome do im\xF3vel"));
+      Imoveis.NomeDonoImovel = prompt("Digite o nome do dono do im\xF3vel", "Nome padr\xE3o do im\xF3vel");
+      Imoveis.QuartosImovel = prompt("Quantos quartos o im\xF3vel possui");
+      Imoveis.BanheirosImovel = prompt("Quantos banheiros o im\xF3vel possui");
+      Imoveis.GaragemImovel = confirm("O im\xF3vel possui uma garagem?\n    Sim\n    N\xE3o");
+
+      if (Imoveis.GaragemImovel === true) {
+        Imoveis.GaragemImovel = "Sim";
+      } else {
+        Imoveis.GaragemImovel = "N\xE3o";
+      }
+
+      alert("Nome do dono do im\xF3vel: ".concat(Imoveis.NomeDonoImovel, "\n      Quantidade de quartos: ").concat(Imoveis.QuartosImovel, "\n      Quantidade de banheiros: ").concat(Imoveis.BanheirosImovel, "\n      Possui uma Garagem: ").concat(Imoveis.GaragemImovel));
       ImoveisCadastrados += 1;
+      var ImovelNomeCase2 = NomeImovel.push(Imoveis.NomeDonoImovel)[ImoveisCadastrados];
       break;
 
     case "2":
-      alert("Imoveis cadastrados: ".concat(Imoveis.NomeImovel));
+      var NomeImoveisCadastrados = alert("Im\xF3veis cadastrados: ".concat(NomeImovel));
       break;
+
+    case "3":
+      alert("Saindo...");
+      break;
+
+    default:
+      alert("Op\xE7\xE3o inv\xE1lida");
   }
 } while (Opcao !== "3");
--- 6. JavaScript I - Lógica de Programação/26. Exercício 8/Cadastro De Imóveis/scripts.js
+++ 6. JavaScript I - Lógica de Programação/26. Exercício 8/Cadastro De Imóveis/scripts.js
@@ -10,24 +10,39 @@ do {
     GaragemImovel: ``,
   }
   Opcao = prompt(`Quantidades de imóveis cadastrados: ${ImoveisCadastrados}
-  1- Cadastrar Imóveis
-  2- Mostrar imóveis cadastrados
-  3- Sair
+  1- Cadastrar Imóveis;
+  2- Mostrar imóveis cadastrados;
+  3- Sair.
   `)
 
   switch (Opcao) {
     case `1`:
-      let NomeDonoImovel = prompt(`Digite o nome do dono do imóvel`)
-      let QuartosImovel = prompt(`Quantos quartos o imóvel possui`)
-      let BanheirosImovel = prompt(`Quantos banheiros o imóvel possui`)
-      let GaragemImovel = prompt(`O imóvel possui uma garagem?
-    1- Sim
-    2- Não`)
-      NomeImovel.push(prompt(`Informe o nome do imóvel`))
+      Imoveis.NomeDonoImovel = prompt(`Digite o nome do dono do imóvel`, `Nome padrão do imóvel`)
+      Imoveis.QuartosImovel = prompt(`Quantos quartos o imóvel possui`)
+      Imoveis.BanheirosImovel = prompt(`Quantos banheiros o imóvel possui`)
+      Imoveis.GaragemImovel = confirm(`O imóvel possui uma garagem?
+    Sim
+    Não`)
+      if (Imoveis.GaragemImovel === true) {
+        Imoveis.GaragemImovel = `Sim`
+      } else {
+        Imoveis.GaragemImovel = `Não`
+      }
+      
+      alert(`Nome do dono do imóvel: ${Imoveis.NomeDonoImovel}
+      Quantidade de quartos: ${Imoveis.QuartosImovel}
+      Quantidade de banheiros: ${Imoveis.BanheirosImovel}
+      Possui uma Garagem: ${Imoveis.GaragemImovel}`)
       ImoveisCadastrados += 1
+      const ImovelNomeCase2 = NomeImovel.push(Imoveis.NomeDonoImovel)[ImoveisCadastrados]
       break;
     case `2`:
-      alert(`Imoveis cadastrados: ${Imoveis.NomeImovel}`)
+      const NomeImoveisCadastrados = alert(`Imóveis cadastrados: ${NomeImovel}`)
       break;
+    case `3`:
+      alert(`Saindo...`)
+      break;
+    default:
+      alert(`Opção inválida`)
    }
 }while(Opcao !== `3`)
