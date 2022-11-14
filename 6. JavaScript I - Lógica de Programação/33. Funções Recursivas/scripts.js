2a6481c17dc047005507c6b7befbfb9c7cd7bb04
Johan <ti.johan@ti7.com.br>
2022-11-14 15:08:13 -0300

    função recursivas

---
+++ 6. JavaScript I - Lógica de Programação/33. Funções Recursivas/scripts.js
@@ -0,0 +1,37 @@
+function Dividir(num) {
+  console.log(num);
+  if (num % 2 === 0) {
+    Dividir(num / 2);
+  } else {
+    return num;
+  }
+}
+
+Dividir(256);
+
+// Primeira -> Segunda -> Terceira -> Quarta
+
+/*
+function Dobrar(num) {
+  console.log(num)
+  Dobrar(num * 2)
+}
+
+Dobrar(1)
+*/
+
+// Fatorial de 5: !5 == 5 * 4 * 3 * 2 * 1 == 5 * !4
+function Fatorial(num) {
+  console.log(`Número: ${num}`);
+  if (num === 0) {
+    // Caso base
+    return 1;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    console.log(`${num} * ${num - 1} `);
+    return num * Fatorial(num - 1);
+  }
+}
+
+console.log(Fatorial(5));
