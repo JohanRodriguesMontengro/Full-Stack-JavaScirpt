dea18ee6f41ab453dc7ca02058157c766655bc45
Johan <ti.johan@ti7.com.br>
2022-12-20 15:04:37 -0300

    objetos globais

---
+++ 6. JavaScript I - Lógica de Programação/37. Objetos Globais/script.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+window.alert("Oi");
+alert("Oi");
+window.console.error("[ERROR]");
+console.error("[ERROR]");
+var pessoa = Object();
+pessoa;
+var lista = Array();
+lista;
+lista = Array("a", "b", "c");
+lista;
+var a = String();
+a;
+String(42);
+String(true);
+String(false);
+String({});
+String(console.log);
+Number("42");
+Number.isInteger(4.5);
+Number.isInteger(4);
+Date();
+Math.PI;
+Math.pow(2, 10);
+Math.sqrt(144);
+Math.random();
---
+++ 6. JavaScript I - Lógica de Programação/37. Objetos Globais/script.js
@@ -0,0 +1,31 @@
+window.alert(`Oi`)
+alert(`Oi`)
+window.console.error(`[ERROR]`)
+console.error(`[ERROR]`)
+
+let pessoa = Object()
+pessoa
+
+let lista = Array()
+lista
+lista = Array(`a`, `b`, `c`)
+lista
+
+let a = String()
+a
+
+String(42)
+String(true)
+String(false)
+String({})
+String(console.log)
+Number(`42`)
+Number.isInteger(4.5)
+Number.isInteger(4)
+
+Date()
+
+Math.PI
+Math.pow(2, 10)
+Math.sqrt(144)
+Math.random()
